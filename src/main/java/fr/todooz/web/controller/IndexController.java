56bef98cf2855ea0ee867637929e0feaa4ab475d
Alexandre Toudic <alexandre.toudic@centrale-marseille.fr>
2013-11-07 16:13:29 +0100

    adding Spring MVC

---
+++ src/main/java/fr/todooz/web/controller/IndexController.java
@@ -0,0 +1,22 @@
+package fr.todooz.web.controller;
+
+import fr.todooz.web.servlet.DummyData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("tasks", DummyData.tasks());
+
+        return "index";
+    }
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello world";
+    }
+}
